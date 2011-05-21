f573e8df5771692938268055a83f9b09cd653ce9
Aziz M. Bookwala <aziz@mantik.in>
2011-05-21 19:55:12 +0530

    Added model forms for question and answer

---
+++ problem3/qanda/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from models import Question, Answer
+
+class QuestionForm(forms.ModelForm):
+    
+    class Meta:
+        model = Question
+        
+class AnswerForm(forms.ModelForm):
+    
+    class Meta:
+        model = Answer
