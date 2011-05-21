9202385fdf4175631cc5e499841d3fab36946e79
Aziz M. Bookwala <aziz@mantik.in>
2011-05-22 05:10:41 +0530

    Ajaxified answer rating

--- problem3/qanda/urls.py
+++ problem3/qanda/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import *
+from models import Answer, Question
 
 urlpatterns = patterns('qanda.views',
     url(r'^$', 'landing_page', name="landing_page"),
     url(r'^question/(?P<qid>\w+)', 'question', name="question_page"),
+    url(r'^rating/answer', 'rating', {'model': Answer}, name="answer_rating"),
+    url(r'^rating/question', 'rating', {'model': Question}, name="question_rating"),
 )
