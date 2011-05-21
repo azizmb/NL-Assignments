a7accd5ba7b7326a8171a17e055c56c540736b33
Aziz M. Bookwala <aziz@mantik.in>
2011-05-21 19:52:06 +0530

    Added models for question and answer

--- problem3/qanda/models.py
+++ problem3/qanda/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PostModel(models.Model):
+    posted_by = models.CharField(max_length=50, blank=True, null=True)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    
+    class Meta:
+        abstract = True
+    
+class Question(PostModel):
+    question = models.TextField(max_length=100)
+    
+    
+class Answer(PostModel):
+    question = models.ForeignKey("Question")
+    answer = models.TextField(max_length=100)
+    
