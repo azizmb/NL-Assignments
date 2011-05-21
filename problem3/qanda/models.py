31900dc04e165096827cd4dd7a96d5de3e4e69cc
Aziz M. Bookwala <aziz@mantik.in>
2011-05-22 02:53:06 +0530

    Changed rating to non-editable

--- problem3/qanda/models.py
+++ problem3/qanda/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class PostModel(models.Model):
     posted_by = models.CharField(max_length=50, blank=True, null=True)
     posted_on = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0, editable=False)
     
     class Meta:
         abstract = True
