a80175d3d5b740dfb0b1fa235e4d04423a15a8e0
Aziz M. Bookwala <aziz@mantik.in>
2011-05-21 19:35:43 +0530

    Added decode function

--- problem2/urlencode.py
+++ problem2/urlencode.py
@@ -11,6 +11,7 @@ _mapping = {}
 for i, c in zip(xrange(256), str(bytearray(xrange(256)))):
     _mapping[c] = '%{:02X}'.format(i)
 
+_reverse_mapping = dict((y[1:], x) for x, y in _mapping.items())
 
 def encode(s, safe=""):
     full_safe = _safe + safe
@@ -18,3 +19,15 @@ def encode(s, safe=""):
     mapping.update(dict([(x, x) for x in full_safe]))
     
     return "".join([mapping[c] for c in s])
+
+def decode(s):
+    tokens = s.split("%")
+    s = tokens[0]
+    if len(tokens)>1:
+        for token in tokens[1:]:
+            try:
+                s = s + _reverse_mapping[token[:2]] + token[2:]
+            except KeyError:
+                s = s + "%" + token
+    return s
+        
