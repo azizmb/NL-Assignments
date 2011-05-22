464eb2646aaef6c908c3855c00c14476989b4589
Aziz M. Bookwala <aziz@mantik.in>
2011-05-22 05:51:55 +0530

    Url to serve static media on debug

--- problem3/qanda/urls.py
+++ problem3/qanda/urls.py
@@ -7,3 +7,11 @@ urlpatterns = patterns('qanda.views',
     url(r'^rating/answer', 'rating', {'model': Answer}, name="answer_rating"),
     url(r'^rating/question', 'rating', {'model': Question}, name="question_rating"),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+                                'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+                    )
