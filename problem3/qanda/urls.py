43b2437c2846dbf721262db5ad51efac3f758705
Aziz M. Bookwala <aziz@mantik.in>
2011-05-22 06:57:47 +0530

    Moved media serving to main urls file

--- problem3/qanda/urls.py
+++ problem3/qanda/urls.py
@@ -7,11 +7,3 @@ urlpatterns = patterns('qanda.views',
     url(r'^rating/answer', 'rating', {'model': Answer}, name="answer_rating"),
     url(r'^rating/question', 'rating', {'model': Question}, name="question_rating"),
 )
-
-from django.conf import settings
-if settings.DEBUG:
-    urlpatterns += patterns('',
-                            url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
-                                'django.views.static.serve',
-                                {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
-                    )
--- problem3/urls.py
+++ problem3/urls.py
@@ -7,3 +7,11 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'^', include("qanda.urls")),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+                                'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+                    )
